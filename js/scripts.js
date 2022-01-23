95ac86b99e7abcd1866ede040302d2fa60551a7b
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-01-23 07:06:40 -0500

    Initial commit old first project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//document.addEventListener('DOMcontentLoaded', () => {} );
+//Para reordenar el banner cuando se está a más de 641px width
+//function restructurebanner(x) {
+//    if (x.matches) { // If media query matches
+//        document.getElementById("promotion-container").prepend(document.getElementById("headline"));
+//        document.getElementById("promotion-container").appendChild(document.getElementById("button-rent"));
+//    } else {
+//        if (document.getElementById("banner-content").contains(document.getElementById("headline"))) {
+//            document.getElementById("banner-content").prepend(document.getElementById("headline"));
+//            document.getElementById("banner-content").appendChild(document.getElementById("button-rent"));
+//        }
+//    }
+//}
+//
+//var x = window.matchMedia("(min-width: 641px)")
+//restructurebanner(x) // Call listener function at run time
+//x.addListener(restructurebanner)
